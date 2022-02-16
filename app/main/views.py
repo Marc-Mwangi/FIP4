c3d7b7bdec4031e63dd7ee9de963274404e493c4
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-02-16 13:24:59 +0300

    First coommit

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template,redirect ,url_for
+from . import main
+from flask_login import login_required
+
+
+# Views
+@main.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+
+    
---
+++ run.py
@@ -0,0 +1,30 @@
+from app import create_app, db
+from flask_script import Manager,Server
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager
+
+from app.models import User
+
+app = create_app('development')
+
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db = db, User= User)
+
+
+if __name__ == '__main__':
+    manager.run()
